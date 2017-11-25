b1887772a7f411b41fc6536e794712e5ecb50534
Unknown <houd.youness@gmail.com>
2017-11-25 00:46:34 +0000

    initier un systeme de tuile

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -2,10 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'page-hello-ionic',
-  templateUrl: 'hello-ionic.html'
+  templateUrl: 'hello-ionic.html',
 })
 export class HelloIonicPage {
-  constructor() {
 
+  posts = [];
+
+  constructor() {
   }
+
+  ionViewDidLoad() {
+    this.posts = [
+      {
+        description: 'Trying out digital painting',
+        image: 'https://image.flaticon.com/icons/svg/607/607621.svg'
+      },
+      {
+        description: 'Trying out digital painting',
+        image: 'https://image.flaticon.com/icons/svg/139/139252.svg'
+      },
+      {
+        description: 'Trying out digital painting',
+        image: 'https://image.flaticon.com/icons/svg/119/119073.svg'
+      },
+      {
+        description: 'Trying out digital painting',
+        image: 'https://image.flaticon.com/icons/svg/139/139168.svg'
+      }
+    ];
+  }
+
 }
